a516bd4ef91a12ffe040bdab1c579778f48d443e
raghu <http://localhost:9000>
2018-06-22 17:03:51 +0530

    #SB-1448 fix : Browser back button click should not take user to the Creation pop up again fixed

--- src/app/client/src/app/modules/workspace/components/create-content/create-content.component.ts
+++ src/app/client/src/app/modules/workspace/components/create-content/create-content.component.ts
@@ -1,10 +1,11 @@
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationStart} from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ResourceService, ConfigService } from '@sunbird/shared';
 import { SuiModule } from 'ng2-semantic-ui/dist';
 import { SuiModalService, TemplateModalConfig, ModalTemplate } from 'ng2-semantic-ui';
 import { FrameworkService, PermissionService } from '@sunbird/core';
 import { IInteractEventInput, IImpressionEventInput } from '@sunbird/telemetry';
+
 @Component({
   selector: 'app-create-content',
   templateUrl: './create-content.component.html',
@@ -45,6 +46,12 @@ export class CreateContentComponent implements OnInit {
   */
   public frameworkService: FrameworkService;
 
+
+  /**
+  * To navigate to other pages
+  */
+  router: Router;
+
   /**
    * reference of permissionService service.
   */
@@ -64,12 +71,20 @@ export class CreateContentComponent implements OnInit {
 
   * @param {ResourceService} resourceService Reference of ResourceService
  */
-  constructor(configService: ConfigService, resourceService: ResourceService,
+  constructor(configService: ConfigService, router: Router, resourceService: ResourceService,
     frameworkService: FrameworkService, permissionService: PermissionService, private activatedRoute: ActivatedRoute) {
     this.resourceService = resourceService;
     this.frameworkService = frameworkService;
     this.permissionService = permissionService;
     this.configService = configService;
+    this.router = router;
+    /**
+      * check for previous route and redirect based on the previous url
+     */
+    this.router.events.filter(e => e instanceof NavigationStart )
+      .bufferCount(2, 1).subscribe(e => {
+        this.checkForPreviousRouteForRedirect(e);
+    });
   }
 
   ngOnInit() {
@@ -91,4 +106,23 @@ export class CreateContentComponent implements OnInit {
       }
     };
   }
+
+  checkForPreviousRouteForRedirect (e) {
+    const previousUrl = e[0]['url'].split('/');
+    const currentUrl = e[1]['url'].split('/');
+     /**
+      * if current url is in /create and previous url is edit/draft then redirect
+     */
+    if (previousUrl.indexOf('edit') !== -1 &&
+        previousUrl.indexOf('draft') !== -1 &&
+          currentUrl.indexOf('create') !== -1) {
+      this.redirect();
+    }
+  }
+
+  redirect() {
+    this.router.navigate(['./create'], {relativeTo: this.activatedRoute.parent});
+  }
+
+
 }
